5daa891107962a1acadf9fcb9803963d2265d022
imosudi <imosudi@gmail.com>
2018-02-03 16:18:28 +0100

    Converted T to 0 M to 1

--- hash.py
+++ hash.py
@@ -1,7 +1,8 @@
 import numpy as np
+from numpy import array
 
-filename = 'example.in'
-#filename = 'small.in'
+#filename = 'example.in'
+filename = 'small.in'
 #filename = 'medium.in'
 
 def googleHash(filename):
@@ -21,7 +22,10 @@ def googleHash(filename):
         #Then, the remaining lines
         countM = 0
         countT = 0
+        rowcount = 0
+        mynumarray = np.random.randn(int(requirements[0]),int(requirements[1]))
         for line in dataset:
+            #line = line.split
             lineList = []
             count = 0
             while count < int(requirements[1]):
@@ -32,7 +36,31 @@ def googleHash(filename):
                 lineList.append(line[count])
                 count+=1
             print lineList
+            for i in range (int(requirements[1])):
+                if lineList[i] == "T" :
+                    lineList[i] = 0
+                if lineList[i] == "M" :
+                    lineList[i] = 1
+            #lineList = np.array(lineList, dtype=str)
+            #print lineList
+            #lineList = lineList.astype(np.float)
+            mynumarray[rowcount] = np.array(lineList, dtype=str)
+            #array2 =np.insert(mynumarray, lineList, axis=count)
+            #mynumarray[0] = np.array(lineList)
+
+            #data_array = array( lineList )
+            #print data_array
+            rowcount+=1
+        print mynumarray
+        #print mynum
+
         print ' %s numbers of \'M\' \n %s numbers of \'T\' ' % (countM, countT)
+
+        #data_array2 = np.loadtxt(filename, dtype=str, skiprows=1)
+        #print data_array2
+        #data = np.loadtxt(filename, delimiter="\n", skiprows=1)
+
+
 
 
 
